be7c6de80dcbff8ec800eae6d010b49ba28762d2
Ant-Fili-2002 <100161181+Ant-Fili-2002@users.noreply.github.com>
2024-09-11 14:54:08 -0500

    Update PokedexDriver.cpp

---
+++ PokedexDriver.cpp
@@ -0,0 +1,60 @@
+///////////////////////////////////
+//Anthony Filigenzi
+//Kylie Truong
+//Jocelyn Lee
+//Adam Yang
+
+//9/11/2024
+
+
+
+
+//Include
+#include <fstream>
+#include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <ctime>
+using namespace std;
+
+void storeInfo(){
+    //Find out the information of the student
+    string studName;
+    string favPokemon;
+    string favStarter;
+    cout << "Name of student: ";
+    cin >> studName;
+    cout << "Name of Favorite Pokemon: ";
+    cin >> favPokemon;
+    cout << "Name of Favorite Starter Pokemon: ";
+    cin >> favStarter;
+
+    //Record it to Files
+    fstream pokedexInfo;
+    pokedexInfo.open("pokedex.txt");
+
+    //Create the file if it doesn't exist
+    if (!pokedexInfo){
+    ofstream pokedexInfo("pokedex.txt");
+    };
+
+
+    //Output the info
+    pokedexInfo << studName << " " << favPokemon << " " << favStarter << endl;
+
+    pokedexInfo.close();
+
+
+}
+
+
+int main(){
+
+    storeInfo();
+
+
+    return 0;
+}
