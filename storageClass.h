b3d1f4982481cb1a1bdaed81b40bc6d1f4164865
Jocelyn Lee <jalee45@tntech.edu>
2024-09-16 13:11:12 -0500

    Update storageClass.h

---
+++ storageClass.h
@@ -0,0 +1,6 @@
+/********************************************************************************
+	Title: 	  storageClass.h
+	Authors:  Anthony Filigenzi, Jocelyn Lee, Kylie Truong & Adam Yang
+	Date:  	  09/11/2024
+	Purpose:  Header file for storageClass.cpp 
+*********************************************************************************/
