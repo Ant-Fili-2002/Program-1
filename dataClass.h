4b4dab308849490b87e4fd337f9cce53b7d7ad13
Jocelyn Lee <jalee45@tntech.edu>
2024-09-16 12:49:30 -0500

    Update dataClass.h

---
+++ dataClass.h
@@ -0,0 +1,6 @@
+/********************************************************************************
+	Title: 		dataClass.h
+	Authors:  Anthony Filigenzi, Jocelyn Lee, Kylie Truong & Adam Yang
+	Date:  		09/11/2024
+	Purpose:  Header file for dataClass.cpp 
+*********************************************************************************/
