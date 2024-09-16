64145debcb7cb76591fc436b74080d8412a6c0ba
Jocelyn Lee <jalee45@tntech.edu>
2024-09-16 13:03:03 -0500

    Update otherClass.h

---
+++ otherClass.h
@@ -0,0 +1,6 @@
+/********************************************************************************
+	Title: 	  otherClass.h
+	Authors:  Anthony Filigenzi, Jocelyn Lee, Kylie Truong & Adam Yang
+	Date:  	  09/11/2024
+	Purpose:  Header file for otherClass.cpp 
+*********************************************************************************/
