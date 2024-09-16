b4d155162a2a93ae95d21fd9a946779443dc6a23
Jocelyn Lee <jalee45@tntech.edu>
2024-09-16 13:03:42 -0500

    Update otherClass.cpp

--- otherClass.cpp
+++ otherClass.cpp
@@ -1,5 +1,5 @@
 /********************************************************************************
-	Title: 	  otherClass.h
+	Title: 	  otherClass.cpp
 	Authors:  Anthony Filigenzi, Jocelyn Lee, Kylie Truong & Adam Yang
 	Date:  	  09/11/2024
 	Purpose:  Median for PokedexDriver.cpp and storage 
