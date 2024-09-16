1235cba95abaeb25a3ba8134513d0a50faaee3bb
Ant-Fili-2002 <100161181+Ant-Fili-2002@users.noreply.github.com>
2024-09-16 14:40:24 -0500

    Update PokedexDriver.cpp

--- PokedexDriver.cpp
+++ PokedexDriver.cpp
@@ -6,8 +6,6 @@
                     functions can be tested, takes user input & ends naturally
 *********************************************************************************/
 
-
-
 //Include
 #include <fstream>
 #include <iostream>
@@ -17,42 +15,99 @@
 #include <string>
 #include <cctype>
 #include <ctime>
+#include "dataClass.h"
 using namespace std;
 
-void storeInfo(){
+//Function for adding to the Pokedex
+void addToPokedex(){
+    PokemonInfo pokeInfo;
     //Find out the information of the student
-    string studName;
-    string favPokemon;
-    string favStarter;
     cout << "Name of student: ";
-    cin >> studName;
+    cin >> pokeInfo.studentName;
     cout << "Name of Favorite Pokemon: ";
-    cin >> favPokemon;
+    cin >> pokeInfo.favPokemon;
     cout << "Name of Favorite Starter Pokemon: ";
-    cin >> favStarter;
+    cin >> pokeInfo.favStarter;
+    cout << endl;
 
     //Record it to Files
     fstream pokedexInfo;
-    pokedexInfo.open("pokedex.txt");
-
-    //Create the file if it doesn't exist
-    if (!pokedexInfo){
-    ofstream pokedexInfo("pokedex.txt");
-    };
+    pokedexInfo.open("pokedex.txt",ios::app);
 
 
     //Output the info
-    pokedexInfo << studName << " " << favPokemon << " " << favStarter << endl;
+    pokedexInfo << "ID Number: " << pokeInfo.idNumber << endl;
+    pokedexInfo << "Student Name: " << pokeInfo.studentName << endl;
+    pokedexInfo << "Favorite Pokemon: " << pokeInfo.favPokemon << endl;
+    pokedexInfo << "Favorite Starter: " << pokeInfo.favStarter << endl;
 
-    pokedexInfo.close();
+}
 
+//Function for Displaying the Pokedex
+void displayPokedex(){
+    ifstream displayText("pokedex.txt");
+
+    if (displayText.is_open()){
+        cout << endl;
+        cout << displayText.rdbuf();
+        cout << endl;
+    }
+
+}
+
+
+void inputFromUser(){
+    int userInput;
+
+    //Loop
+    do{
+    cout << "What would you like to do?" << endl;
+    cout << "1. Display PokeDex" << endl;
+    cout << "2. Add to PokeDex" << endl;
+    cout << "3. Remove from PokeDex" << endl;
+    cout << "4. Exit" << endl;
+    cin >> userInput;
+
+    //if user wishes to display the PokeDex
+    if (userInput == 1){
+       displayPokedex();
+    }
+    //if user wihses to add to the PokeDex
+    else if (userInput == 2) {
+        addToPokedex();
+    }
+    //if user wishes to remove from PokeDex
+    else if (userInput == 3){
+        
+    }
+    else {
+        cout << "Sorry, that isn't an option" << endl;
+    }
+    }while (userInput != 4);
+
+    cout << "Goodbye!" << endl;
+    cout << endl;
 
 }
 
 
 int main(){
+    
 
-    storeInfo();
+    //Make sure the pokedex file exists
+    fstream pokeDexFile;
+
+    pokeDexFile.open("pokedex.txt");
+    //Create it if it doesn't exist
+    if (!pokeDexFile){
+    ofstream pokedexInfo("pokedex.txt");
+    };
+    //Greet the user
+    cout << "Hello! And welcome to the TN Tech PokeDex!" << endl;
+    cout << "------------------------------------------" << endl;
+    
+    //Main Function
+    inputFromUser();
 
 
     return 0;
