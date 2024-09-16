fa3bc2aa9a62119ed18370c68791df67e7da6906
VesperaVela <kntruong43@tntech.edu>
2024-09-16 15:21:21 -0500

    Update otherClass.h with "bare bones"

--- otherClass.h
+++ otherClass.h
@@ -4,3 +4,20 @@
 	Date:  	  09/11/2024
 	Purpose:  Header file for otherClass.cpp 
 *********************************************************************************/
+#ifndef OTHERCLASS_H
+#define OTHERCLASS_H
+
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cctype>
+#include <ctime>
+#include <PokedexDriver.h>
+
+using namespace std;
+
+int main();
+
+
+#endif
