96eb71dadb94101dc7600ef3979e143d2813043e
Jocelyn Lee <jalee45@tntech.edu>
2024-09-16 12:46:53 -0500

    Update dataClass.cpp

--- dataClass.cpp
+++ dataClass.cpp
@@ -2,5 +2,6 @@
 	Title: 	 dataClass.cpp
 	Authors: Anthony Filigenzi, Jocelyn Lee, Kylie Truong & Adam Yang
 	Date:  	 09/11/2024
-	Purpose: Points to array of pointers to the data.
+	Purpose: Stores the information of students, their favorite 
+ 	         Pokemon and favorite starter Pokemon.
 *************************************************************************/
