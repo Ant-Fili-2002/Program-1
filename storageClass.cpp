cdf251de50ac0118d4ae98f77bb5f6160759a1a0
Ant-Fili-2002 <100161181+Ant-Fili-2002@users.noreply.github.com>
2024-09-23 15:08:32 -0500

    Update storageClass.cpp

--- storageClass.cpp
+++ storageClass.cpp
@@ -4,12 +4,22 @@
 	Date:  	  09/11/2024
 	Purpose:  Pointer to an array of pointers to the data in the data class. 
 *********************************************************************************/
-
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <csdLib>
+#include "storageClass.h"
+#include <cctype>
+#include <ctime>
+#include "storageClass.h"
+#include "PokedexDriver.h"
 
-using namespace std
+#define numPointers 4
 
+using namespace std;
 
+string *array_name[pokeMax];
+string *array_poke[pokeMax];
+string *array_starter[pokeMax];
+string* (*array_name)[pokeMax] = &array_name;
+string* (*pArray_poke)[pokeMax] = &array_poke;
+string* (*pArray_starter)[pokeMax] = &array_starter;
