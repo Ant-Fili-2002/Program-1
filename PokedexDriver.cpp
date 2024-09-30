3469f5d4adbfda3866eb6ba664ddaeafdec500de
Ant-Fili-2002 <100161181+Ant-Fili-2002@users.noreply.github.com>
2024-09-30 14:42:09 -0500

    PokedexDriver.cpp 2.0

--- PokedexDriver.cpp
+++ PokedexDriver.cpp
@@ -10,39 +10,81 @@
 #include "dataClass.h"
 #include "PokedexDriver.h"
 
-using namespace std;
 
-//Count number of Pokemon in File
+int numIDS = 0;
+PokemonInfo pokeInfo;
+
+
+//Count how many entries there are on Pokedex
 void pokedexCount(){
     string line;
-    int lineTotal = 1;
+    int lineTotal = 0;
+    int lineRead = 0;
+    string s;
+    int studentInfo = 0;
 
 
     fstream pokedexCount;
     pokedexCount.open("pokedex.txt");
-    while(!pokedexCount.eof()) {
-	getline(pokedexCount, line);
-	lineTotal ++;
-}
-    numIDs = (lineTotal / 4) + 1;
+    while(!pokedexCount.eof()){
+    getline(pokedexCount, line);
+    getline(pokedexCount, pokeInfo.studentName[studentInfo]);
+    getline(pokedexCount, pokeInfo.favPokemon[studentInfo]);
+    getline(pokedexCount, pokeInfo.favStarter[studentInfo]);
+    lineTotal ++;
+    studentInfo ++;
+    }
+
+    numIDs = lineTotal;
+
+
+
+    pokedexCount.close();
 
 
 }
+
 
 //Function for adding to the Pokedex
 void addToPokedex(){
-   //Output the info
+    //Create array for Class
+    
+    //Find out the information of the student
+    cout << "Name of student: ";
+    cin >> pokeInfo.studentName[numIDs];
+    cout << "Name of Favorite Pokemon: ";
+    cin >> pokeInfo.favPokemon[numIDs];
+    cout << "Name of Favorite Starter Pokemon: ";
+    cin >> pokeInfo.favStarter[numIDs];
+    cout << endl;
+
+    //Record it to Files
+    fstream pokedexInfo;
+    pokedexInfo.open("pokedex.txt", std::ios_base::app);
+
+
+    //Output the info
     pokedexInfo << "ID Number: " << numIDs << endl;
-    pokedexInfo << "Student Name: " << pokeInfo.studentName << endl;
-    pokedexInfo << "Favorite Pokemon: " << pokeInfo.favPokemon << endl;
-    pokedexInfo << "Favorite Starter: " << pokeInfo.favStarter << endl;
+    pokedexInfo << "Student Name: " << pokeInfo.studentName[numIDs] << endl;
+    pokedexInfo << "Favorite Pokemon: " << pokeInfo.favPokemon[numIDs] << endl;
+    pokedexInfo << "Favorite Starter: " << pokeInfo.favStarter[numIDs] << endl;
+
+    pokedexInfo.close();
+
+    numIDs++;
 }
 
 //Function for Displaying the Pokedex
 void displayPokedex(){
     ifstream displayText("pokedex.txt");
+    int length;
 
-    if (displayText.is_open()){
+    if (filesystem::file_size("pokedex.txt") == 0){
+        cout << "NO ENTRIES FOUND" << endl;
+        cout << endl;
+        displayText.close();
+    }
+    else {
         cout << endl;
         cout << displayText.rdbuf();
         cout << endl;
@@ -50,18 +92,58 @@ void displayPokedex(){
 
 }
 
+void deletePokemon(int numPokemon, int pokemonChoice){
+fstream pokeDelete;
+pokeDelete.open("pokedex.txt", std::ios::out);
+
+//If it's the highest number
+    if (numPokemon == pokemonChoice){
+        string lineRandom;
+        //Loop until you find the right line
+        for (int j = 0; j < (numPokemon*4)-4; j++){
+            getline(pokeDelete, lineRandom);
+        }
+        pokeDelete << "";
+        pokeDelete << "";
+        pokeDelete << "";
+        pokeInfo.studentName[numPokemon] = "";
+        pokeInfo.favPokemon[numPokemon] = "";
+        pokeInfo.favStarter[numPokemon] = "";
+
+    }
+    else if (numPokemon > pokemonChoice){
+    for (int i = pokemonChoice; i < numPokemon; ++i){
+    pokeInfo.studentName[i] = pokeInfo.studentName[i + 1];
+    pokeInfo.favPokemon[i] = pokeInfo.favPokemon[i + 1];
+    pokeInfo.favStarter[i] = pokeInfo.favStarter[i + 1];
+    }
+    }
+    numIDS--;
+
+    for (int q = 1; q < numPokemon - 1; q++){
+    pokeDelete << "ID Number: " << q << endl;
+    pokeDelete << "Student Name: " << pokeInfo.studentName[q] << endl;
+    pokeDelete << "Favorite Pokemon: " << pokeInfo.favPokemon[q] << endl;
+    pokeDelete << "Favorite Starter: " << pokeInfo.favStarter[q] << endl;
+    }
+    pokeDelete.close();
+
+}
+
 
 void inputFromUser(){
+   
     int userInput;
-
+    
     //Loop
     do{
-    cout << "What would you like to do?";
+    cout << "What would you like to do?" << endl;
     cout << "1. Display PokeDex" << endl;
     cout << "2. Add to PokeDex" << endl;
     cout << "3. Remove from PokeDex" << endl;
     cout << "4. Exit" << endl;
     cin >> userInput;
+    cin.ignore();
 
     //if user wishes to display the PokeDex
     if (userInput == 1){
@@ -73,6 +155,11 @@ void inputFromUser(){
     }
     //if user wishes to remove from PokeDex
     else if (userInput == 3){
+        int pokeDelete;
+        displayPokedex();
+        cout << "Which pokemon entry would you like to delete?" << endl;
+        cin >> pokeDelete;
+        deletePokemon(numIDs, pokeDelete);
         
     }
     else if (userInput == 4){
@@ -88,8 +175,10 @@ void inputFromUser(){
 }
 
 
+
+
+
 int main(){
-    
 
     //Make sure the pokedex file exists
     fstream pokeDexFile;
@@ -102,9 +191,12 @@ int main(){
     //Greet the user
     cout << "Hello! And welcome to the TN Tech PokeDex!" << endl;
     cout << "------------------------------------------" << endl;
+    //Count number of lines
+    pokedexCount();
     
     //Main Function
     inputFromUser();
+    
 
 
     return 0;
