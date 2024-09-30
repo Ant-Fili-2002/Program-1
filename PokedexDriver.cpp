a6fc01f53356ac34d27980571d17ef8ddacfa684
Jocelyn Lee <jalee45@tntech.edu>
2024-09-30 15:20:30 -0500

    update int main() (#8)

--- PokedexDriver.cpp
+++ PokedexDriver.cpp
@@ -174,30 +174,58 @@ void inputFromUser(){
 
 }
 
-
-
-
-
-int main(){
-
+//****************************************************************************
+//                                 main()
+//
+// task:          Contains the user menu and calls onto the other functions 
+//                based off of the user input.
+//****************************************************************************
+int main()
+{
+    int endProgram = 0;
+	
     //Make sure the pokedex file exists
     fstream pokeDexFile;
-
     pokeDexFile.open("pokedex.txt");
+    
     //Create it if it doesn't exist
-    if (!pokeDexFile){
-    ofstream pokedexInfo("pokedex.txt");
+    if (!pokeDexFile)
+    {
+	ofstream pokedexInfo("pokedex.txt");
     };
-    //Greet the user
+	
+    /*Main Menu Options*/
     cout << "Hello! And welcome to the TN Tech PokeDex!" << endl;
     cout << "------------------------------------------" << endl;
-    //Count number of lines
-    pokedexCount();
-    
-    //Main Function
-    inputFromUser();
-    
+    do{
+    cout << "Welcome To The World of Pokemon! What Would You Like to Do?";
+    cout << "1. Display PokeDex" << endl;
+    cout << "2. Add to PokeDex" << endl;
+    cout << "3. Remove from PokeDex" << endl;
+    cout << "4. Exit" << endl;
+    	cin >> userInput;
 
+    switch(userInput)
+	/*Displays Pokedex*/
+	case 1:
+	    displayPokedex();
+	    break;
+	/*Adds New Student to Pokedex*/
+	case 2:
+	    addToPokedex();
+	    break;
+	/*deletes Pokedex*/
+	case 3:
+	    deletePokedex();
+	    break;
+	/*Ends Program*/
+	case 4:
+	    endProgram = 1;
+	    break;
+    }while (endProgram != 1);
 
+    cout << "See Ya Next Time!" << endl;
+    cout << endl;
+    /*Program 1 Complete :)*/
     return 0;
 }
