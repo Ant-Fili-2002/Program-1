4cdda665f72a6eee29cb66013b96c13d86013d19
Jocelyn Lee <jalee45@tntech.edu>
2024-10-02 15:29:11 -0500

    Update PokedexDriver.cpp

--- PokedexDriver.cpp
+++ PokedexDriver.cpp
@@ -11,56 +11,6 @@
 #include "PokedexDriver.h"
 
 
-int numIDS = 0;
-Pokedex pokeInfo;
-
-
-//Count how many entries there are on Pokedex
-void pokedexCount(){
-    string line;
-    int lineTotal = 0;
-    int lineRead = 0;
-    string s;
-    int studentInfo = 0;
-
-
-    fstream pokedexCount;
-    pokedexCount.open("pokedex.txt");
-    while(!pokedexCount.eof()){
-    getline(pokedexCount, line);
-    getline(pokedexCount, pokeInfo.studentName[studentInfo]);
-    getline(pokedexCount, pokeInfo.favPokemon[studentInfo]);
-    getline(pokedexCount, pokeInfo.favStarter[studentInfo]);
-    lineTotal ++;
-    studentInfo ++;
-    }
-
-    numIDs = lineTotal;
-
-
-
-    pokedexCount.close();
-
-
-}
-
-//Function for Displaying the Pokedex
-void displayPokedex(){
-    ifstream displayText("pokedex.txt");
-    int length;
-
-    if (filesystem::file_size("pokedex.txt") == 0){
-        cout << "NO ENTRIES FOUND" << endl;
-        cout << endl;
-        displayText.close();
-    }
-    else {
-        cout << endl;
-        cout << displayText.rdbuf();
-        cout << endl;
-    }
-
-}
 //****************************************************************************
 //                                 main()
 //
