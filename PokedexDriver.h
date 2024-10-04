4901f3de57b9ab31e793937f51ee93f5fe079a9a
Jocelyn Lee <jalee45@tntech.edu>
2024-10-03 20:41:21 -0500

    Update PokedexDriver.h

--- PokedexDriver.h
+++ PokedexDriver.h
@@ -4,7 +4,6 @@
 	Date:  	    09/11/2024
 	Purpose:    Header file for PokedexDriver.cpp
 *********************************************************************************/
-
 #ifndef POKEDEXDRIVER_H
 #define POKEDEXDRIVER_H
 
@@ -20,18 +19,12 @@
 
 using namespace std;
 
-//Number of entries is automatically 0 until told otherwise
 int numIDs = 1;
-//Number of max entries is just like the first gen number of pokemon
-const int maxPokemon = 150;
 
-//Function Prototypes:
+int main();
+void pokedexCount();
 void addToPokedex();
 void displayPokedex();
-void inputFromUser();
-void deletePokemon(int numPokemon, int pokemonChoice);
-void pokedexCount();
-//Main
-int main();
+void deletePokemon(int,int);
 
 #endif
